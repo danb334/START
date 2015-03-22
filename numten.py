524adf852ebb51958f97b1f2781a281ca2873bbb
danb334 <danb334@hotmail.com>
2015-03-21 21:40:46 -0400

    TESTTESTTEST

---
+++ numten.py
@@ -0,0 +1,8 @@
+print "how old are you?"
+age = raw_input()
+print "how much do you own?"
+Own = raw_input()
+print "sup nig"
+nig = raw_input()
+
+print "so, you're %r old, %r own, and %r nig." % (age, Own, nig)
